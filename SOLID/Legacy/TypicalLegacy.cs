3e22faafe4a46c668716db9493ccdcc732553734
MrBaldman <gpharwood@gmail.com>
2014-05-22 11:24:07 +0100

    method examples

--- SOLID/Legacy/TypicalLegacy.cs
+++ SOLID/Legacy/TypicalLegacy.cs
@@ -4,19 +4,32 @@ namespace SOLID.Legacy
 {
     public class TypicalLegacy
     {
-        public void ProcessIncident(Account account, int incidentId, SqlConnection connection, SqlTransaction transaction)
+        public void ProcessIncident(Account account, int incidentId, 
+			SqlConnection connection, SqlTransaction transaction)
         {
+
+
             if (account.AccountType == "VIP")
             {
+
                 var escalator = new AccountEscalator();
                 escalator.Escalate(account);
+	            return;
             }
+
+
 
             var dataAccess = new DataAccess();
             dataAccess.CreateIncident(account, incidentId, connection);
 
-            if(transaction != null)
-                transaction.Commit();
+
+
+	        if (transaction != null)
+	        {
+
+		        transaction.Commit();
+
+	        }
         }
     }
 }
